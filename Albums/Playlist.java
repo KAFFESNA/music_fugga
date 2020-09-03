5fcca0fb192c6d87f52d6c5d3c5aa8ec816e6778
KAFFESNA <52386711+KAFFESNA@users.noreply.github.com>
2020-09-03 14:37:11 +1200

    v0.09 - Recommendations in Graphics Pane
    In this version I have added:
     - a recommendation system based on a score of 2 or 3
     - Graphics Pane printing of recommendations
     - Stars in the graphics pane to represent the scoring system
     - Clickable stars to rate a recommendation
     - The ability for multiple recommendations to be iterated through using a for loop
     - Storing the rating posted from the stars in the Graphics Pane
    There are still some bugs that I need to iron out, but this is big progress and is a big part of the code.

--- Albums/Playlist.java
+++ Albums/Playlist.java
@@ -25,13 +25,21 @@ public class Playlist{
     private HashMap<String, Album> albumList;
     ArrayList<String> albumKeys = new ArrayList<String>();
     private int albumCounter;
-    private int printCounter;
     private boolean endPrint;
     //constants for the rating systems bounds
     private final int INITIALRATE = 0;
     private final int MINRATE = 1;
     private final int MAXRATE = 3;
     private final int PRINTSTRING = 100;
+    //variables for stars
+    private static final int MAXSTARS = 3;
+    private Star[] stars = new Star[MAXSTARS];
+    //positioning the stars
+    private static final int LEFT = 100;
+    private static final int TOP = 300;
+    private static int starRating = 0;
+    private static String starName = "";
+    private boolean ratedAlbum = false;
     /**
      * Constructor for objects in the class; "Albums"
      */
@@ -152,31 +160,32 @@ public class Playlist{
      */
     public void albumRating(){
         UI.clearText();
+        UI.clearGraphics();
         String rateName = UI.askString("What Album would you like to Rate? ");
         try {
             int rat = albumList.get(rateName).getRating();
-            if (rat == INITIALRATE){
+           if (rat == INITIALRATE){
                 UI.println(rateName + " is currently not yet rated");
-            } else {
+           } else {
                 UI.println(rateName + " currently has a rating of: " + rat + "/3 stars.");
-            }
-            try {
+           }
+           try {
                 int newRating = UI.askInt("How many stars would you rate " + rateName + " ? (1-3 stars) ");
                 if (newRating < MINRATE || newRating > MAXRATE) {
                     throw new ArithmeticException();
                 } else {
                     newRating = albumList.get(rateName).updateRating(newRating);
                     String ratString = albumList.get(rateName).assignRate(newRating);
-                    recommendAlbum(newRating, rateName);
+                    String checkGen = albumList.get(rateName).getGenre();
                     UI.println(rateName + "'s rating has changed to: " + newRating + " - " + ratString);
+                    if(newRating >= 2){
+                        recommendAlbum(newRating, rateName, checkGen);
+                    }
                 }
-            } catch (ArithmeticException incorrectStars) {
+           } catch (ArithmeticException incorrectStars) {
                 //Runs if the user enters a number out of range
                 UI.println("Please enter a number between 1 - 3");
-            } catch (Exception invalidStars) {
-                //Runs if the user enters anything other than a number
-                UI.println("Please input a number");
-            }
+           }
         } catch (Exception albumMissing) {
             //Runs if user inputs an album that does not exist in the database
             UI.println("That album is not in our database");
@@ -186,25 +195,76 @@ public class Playlist{
     /**
      * Will recommend an album to the user based on the rating and the genre
      */
-    public void recommendAlbum(int rat, String rateName){
-        printCounter = 0;
+    public void recommendAlbum(int rat, String rateName, String checkGen){
         if (rat > 1){
             for (String name : albumList.keySet()){
                 int recRating = albumList.get(name).getRating();
-                if (recRating == 0 && name != rateName){
+                String rateGen = albumList.get(name).getGenre();
+                if ((recRating == 0) && (name != rateName) && (checkGen.equals(rateGen))){
                     albumKeys.add(name);
                 }
             }
         }
-        String name = albumKeys.get(printCounter);
-        String art = albumList.get(albumKeys.get(printCounter)).getArtist();
-        int pub = albumList.get(albumKeys.get(printCounter)).getYear();
-        String gen = albumList.get(albumKeys.get(printCounter)).getGenre();
-        int printRat = albumList.get(albumKeys.get(printCounter)).getRating();
-        String ratString = albumList.get(rateName).assignRate(printRat);
-        UI.drawString("You might also like:", PRINTSTRING, PRINTSTRING);
-        UI.drawString(name + " by " + art, PRINTSTRING, PRINTSTRING + 25);
-        UI.drawString(pub + "     " + printRat + " - " + ratString, PRINTSTRING, PRINTSTRING + 50);
+        int printArr = albumKeys.size();
+        try{
+            if (printArr == 0){
+                //will throw an error if no albums are stored in the arraylist
+                throw new ArithmeticException();
+            } else {
+                for (int printCounter = 0; printCounter <= printArr; printCounter++){
+                    ratedAlbum = false;
+                    UI.clearGraphics();
+                    String name = albumKeys.get(printCounter);
+                    String art = albumList.get(albumKeys.get(printCounter)).getArtist();
+                    int pub = albumList.get(albumKeys.get(printCounter)).getYear();
+                    String gen = albumList.get(albumKeys.get(printCounter)).getGenre();
+                    int printRat = albumList.get(albumKeys.get(printCounter)).getRating();
+                    String ratString = albumList.get(rateName).assignRate(printRat);
+                    UI.drawString("Displaying:" + (printCounter + 1) + "/" + printArr, PRINTSTRING, PRINTSTRING - 50);
+                    UI.drawString("You might also like:", PRINTSTRING, PRINTSTRING);
+                    UI.drawString(name + " by " + art, PRINTSTRING, PRINTSTRING + 25);
+                    UI.drawString(pub + "     " + printRat + " - " + ratString, PRINTSTRING, PRINTSTRING + 50);
+                    while (ratedAlbum == false){
+                        stars(name);
+                    }
+                }
+            }
+        } catch (ArithmeticException noRecc) {
+            //This will run if there is no recommendation stored in the ArrayList
+            UI.drawString("No recommendations available in the hashmap",PRINTSTRING, PRINTSTRING);
+        } 
+    }
+    
+    /**
+     * will create stars in the GUI
+     */
+    public void stars(String rateName) {
+        //fills the stars array
+        for (int starCounter = 0; starCounter < MAXSTARS; starCounter++){
+            stars[starCounter] = new Star(LEFT * (starCounter + 1), TOP);
+        }
+        starName = rateName;
+        UI.setMouseListener(this::manageMouse);
+    }
+
+    /**
+     * Checks for mouse input
+     */
+    public void manageMouse(String mouseAction, double x, double y){
+        if (mouseAction.equals("clicked")){
+            for (int starCounter = 0; starCounter < MAXSTARS; starCounter++){
+                if (this.stars[starCounter].onStar(x, y) == true){
+                    starRating = (starCounter + 1);
+                    SendRatingBack();
+                    ratedAlbum = true;
+                }
+            }
+        }
+    }
+    public void SendRatingBack(){
+        int printrat = albumList.get(starName).updateRating(starRating);
+        String ratString = albumList.get(starName).assignRate(starRating);
+        UI.println(starName + "'s rating has changed to: " + starRating + " - " + ratString);
     }
 }
 
